1514fb5e11c074446f8b9ed7c916ad995d9ba9f0
Isaiah Dawkins <isaiah.dawkins12@gmail.com>
2024-01-14 11:34:03 -0800

    Testing for mobile

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const splash = document.querySelector('.splash');
 
 /*
@@ -25,31 +24,3 @@ document.addEventListener('DOMContentLoaded', (e) => {
         splash.classList.add('display-none');
     }
 });
-=======
-const splash = document.querySelector('.splash');
-
-/*
-document.addEventListener('DOMContentLoaded', (e)=>{
-    setTimeout(()=>{
-        splash.classList.add('display-none');
-    }, 2000);
-})
-*/
-document.addEventListener('DOMContentLoaded', (e) => {
-    // Check if the splash page has been seen before using sessionStorage
-    const hasSeenSplash = sessionStorage.getItem('hasSeenSplash');
-
-    if (!hasSeenSplash) {
-        // If not seen, show the splash page
-        setTimeout(() => {
-            splash.classList.add('display-none');
-
-            // Set a sessionStorage item to indicate that the splash page has been seen
-            sessionStorage.setItem('hasSeenSplash', 'true');
-        }, 2000); // Adjust the duration based on your preference
-    } else {
-        // If seen before, hide the splash page immediately
-        splash.classList.add('display-none');
-    }
-});
->>>>>>> fe510d61813c0630a27043fab83204a60cb97a11
